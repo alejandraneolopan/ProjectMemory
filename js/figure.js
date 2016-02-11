629917e345f6560836e1a39ff9bbfa57159b18b8
Alejandra Neolopan <alejandra.neolopan@fundacion-jala.org>
2016-02-11 12:58:57 -0400

    Adding documentation to Figure Class

--- js/figure.js
+++ js/figure.js
@@ -1,13 +1,16 @@
 /**
  * Created by SamuelSahonero on 2/4/2016.
  */
-/*
- *  Class Cell save the content of cells
- *  This class creates a new cells matrix randomly
+/*  @class Figure: Save the content of cells
+ *  isEmpty {boolean} property that indicates if the value is empty, this state
+ *       changes when the character property is set.
+  * isHidden {boolean} property that indicates the first time the character value
+   *     can't displayed.
+  * hiddenCharacter {char} property is the character displayed when the state isHidden is true
+  * character {char} property is the value of figure e.g. A,?, ...
  */
-function Figure(x,y){
-    this.row =x;
-    this.column =y;
+
+function Figure(){
     this.isEmpty = true;
     this.isHidden = true;
     this.hiddenCharacter = '*';
@@ -15,29 +18,42 @@ function Figure(x,y){
 }
 
 /*
- * This method returns
+ * @return {char} The actual value of Figure Character
  */
 
 Figure.prototype.getCharacter = function(){
     return this.character;
 };
-
+/*
+ * @param {char|string} character This param set the Figure character value
+ */
 Figure.prototype.setCharacter = function(character){
     this.character = character;
     this.setState(false);
 };
+/*
+ * @return {boolean} The state of Figure Character is empty
+ */
 
 Figure.prototype.isEmptyCell = function(){
     return this.isEmpty;
 };
-
+/*
+ * @param {bool} state This param set the Empty State
+ */
 Figure.prototype.setState = function(state){
     this.isEmpty = state;
 };
+/*
+ * @return {boolean} The state of Figure Character is hidden
+ */
 
 Figure.prototype.showState = function(){
     return this.isHidden;
 };
+/*
+* @param {bool} state This param set the Hidden State
+ */
 Figure.prototype.setShowState = function(state){
     this.isHidden = state;
 };
